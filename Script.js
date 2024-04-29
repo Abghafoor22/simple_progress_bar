23439f5620439e42fe6d05653a2841e9ea17784e
Abdul Ghafoor <abghafoorkandhro@gmail.com>
2024-04-29 19:53:16 +0500

    first commit

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 let precent = document.querySelector('.precent');
 let progress = document.querySelector('.progress');
-let count = 4;
-let per = 16;
+let count = 0;
+let per = 0;
 let loading = setInterval(animate,20)
 function animate(){
-    if (count == 80 && per == 320) {
+    if (count == 70) {
         console.log("hi ima working");
         clearInterval(loading)
     }else{
